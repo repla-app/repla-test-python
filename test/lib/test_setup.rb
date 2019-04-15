49133cc4779a9f2546249fdffb33c45d44b87fa8
Roben Kleene <contact@robenkleene.com>
2019-04-15 15:49:58 -0400

    Fix path

--- test/lib/test_setup.rb
+++ test/lib/test_setup.rb
@@ -2,4 +2,4 @@ require_relative '../bundle/bundler/setup'
 require 'repla/test'
 require Repla::Test::HELPER_FILE
 
-SERVER_BUNDLE_COMMAND = 'repla server'.freeze
+SERVER_BUNDLE_COMMAND = '/usr/local/bin/repla server'.freeze
