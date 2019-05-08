822e85f0c4e0d347a22f52a369a13fda247a4a0f
Roben Kleene <contact@robenkleene.com>
2019-05-08 13:13:05 -0400

    Add Django (#2)
    * Add Django submodule
    
    * Flatten test directory
    
    * Setup Django
    
    * Update linter
    
    * Add to Rakefile
    
    * Bump Django

--- test/tc_django.rb
+++ test/tc_django.rb
@@ -1,15 +1,21 @@
 #!/System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/ruby
 
 require 'minitest/autorun'
-require_relative '../lib/test_setup'
-
-JUPYTER_COMMAND = 'jupyter notebook --no-browser'.freeze
-JUPYTER_HTML_TITLE = 'Home'.freeze
+require_relative 'lib/test_setup'
 
 # Test server
 class TestServer < Minitest::Test
+  EXTERNAL_DIRECTORY = File.expand_path(
+    File.join(__dir__, '../external/repla-test-django/')
+  )
+  EXTERNAL_COMMAND = 'python3 manage.py runserver'.freeze
+  HTML_TITLE = 'Django: the Web framework for perfectionists with deadlines.'
+               .freeze
+
   def setup
-    `#{SERVER_BUNDLE_COMMAND} "#{JUPYTER_COMMAND}"`
+    Dir.chdir(EXTERNAL_DIRECTORY) do
+      `#{SERVER_BUNDLE_COMMAND} "#{EXTERNAL_COMMAND}"`
+    end
     window_id = nil
     Repla::Test.block_until do
       window_id = Repla::Test::Helper.window_id
@@ -23,13 +29,13 @@ class TestServer < Minitest::Test
     @window.close
   end
 
-  def test_jupyter
+  def test_django
     javascript = File.read(Repla::Test::TITLE_JAVASCRIPT_FILE)
     result = nil
     Repla::Test.block_until do
       result = @window.do_javascript(javascript)
-      result == JUPYTER_HTML_TITLE
+      result == HTML_TITLE
     end
-    assert_equal(JUPYTER_HTML_TITLE, result)
+    assert_equal(HTML_TITLE, result)
   end
 end
---
+++ test/tc_jupyter.rb
@@ -0,0 +1,34 @@
+#!/System/Library/Frameworks/Ruby.framework/Versions/2.3/usr/bin/ruby
+
+require 'minitest/autorun'
+require_relative 'lib/test_setup'
+
+# Test server
+class TestServer < Minitest::Test
+  JUPYTER_COMMAND = 'jupyter notebook --no-browser'.freeze
+  JUPYTER_HTML_TITLE = 'Home'.freeze
+  def setup
+    `#{SERVER_BUNDLE_COMMAND} "#{JUPYTER_COMMAND}"`
+    window_id = nil
+    Repla::Test.block_until do
+      window_id = Repla::Test::Helper.window_id
+      !window_id.nil?
+    end
+    refute_nil(window_id)
+    @window = Repla::Window.new(window_id)
+  end
+
+  def teardown
+    @window.close
+  end
+
+  def test_jupyter
+    javascript = File.read(Repla::Test::TITLE_JAVASCRIPT_FILE)
+    result = nil
+    Repla::Test.block_until do
+      result = @window.do_javascript(javascript)
+      result == JUPYTER_HTML_TITLE
+    end
+    assert_equal(JUPYTER_HTML_TITLE, result)
+  end
+end
